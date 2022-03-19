a9896c57e9dc810f3e7585cca3cacf084d7322d6
Ryan-Andrade <94945327+Ryan-Andrade@users.noreply.github.com>
2022-03-19 11:30:57 -0700

    Deliverable 2 commit.

--- scraping.py
+++ scraping.py
@@ -1,4 +1,5 @@
 # Import Splinter, BeautifulSoup, and Pandas
+from re import A
 from splinter import Browser
 from bs4 import BeautifulSoup as soup
 import pandas as pd
@@ -12,6 +13,7 @@ def scrape_all():
     browser = Browser('chrome', **executable_path, headless=True)
 
     news_title, news_paragraph = mars_news(browser)
+    hemisphere_image_urls = hemi_scrape(browser)
 
     # Run all scraping functions and store results in a dictionary
     data = {
@@ -19,7 +21,8 @@ def scrape_all():
         "news_paragraph": news_paragraph,
         "featured_image": featured_image(browser),
         "facts": mars_facts(),
-        "last_modified": dt.datetime.now()
+        "last_modified": dt.datetime.now(),
+        "hemisphere_images": hemisphere_image_urls
     }
 
     # Stop webdriver and return data
@@ -97,7 +100,35 @@ def mars_facts():
     # Convert dataframe into HTML format, add bootstrap
     return df.to_html(classes="table table-striped")
 
+def hemi_scrape(browser):
+    # 1. Use browser to visit the URL 
+    url = 'https://marshemispheres.com/'
+
+    browser.visit(url)
+
+    # 2. Create a list to hold the images and titles.
+    hemisphere_image_urls = []
+
+    # 3. Write code to retrieve the image urls and titles for each hemisphere.
+    h3_list = [0, 1, 2, 3]
+
+    for hemisphere in h3_list:
+        browser.find_by_tag('h3')[hemisphere].click()
+        html = browser.html
+        hemi_soup = soup(html, 'html.parser')
+        title = hemi_soup.find('h2').get_text()
+        browser.find_by_tag('a')[3].click()
+        browser.windows[0].close()
+        html2 = browser.html
+        hemi_img = soup(html2, 'html.parser')
+        img= hemi_img.find('img').get('src')
+        hemi_dict = {title : img}
+        hemisphere_image_urls.append(hemi_dict)
+        browser.visit(url)
+    return hemisphere_image_urls
+
 if __name__ == "__main__":
 
     # If running as script, print scraped data
     print(scrape_all())
+
